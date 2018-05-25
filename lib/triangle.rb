8c50e2987b21334c810db438537fdcc7b5aca896
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-25 21:59:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,8 +12,6 @@ class Triangle
     if side_1 + side_2 <= side_3 || side_2 + side_3 <= side_1 || side_1 + side_3 <= side_2
       begin
         raise TriangleError
-      rescue TriangleError => error
-        puts error.message
       end
     elsif side_1 == side_2 && side_1 == side_3
       :equilateral
