f6a93fc6255ade07dca1891a4e13fcdcdc36d9c8
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-25 21:38:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+
+  def initialize(side_1, side_2, side_3)
+
+  end
+
+  def kind
+    if side_1 == side_2 == side_3
+      :equilateral
+    elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
 end
